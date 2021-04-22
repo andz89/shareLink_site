d57259c9947347bd402c3880e187c9051f01793d
andz <andzrivero89@gmail.com>
2021-04-21 22:33:55 -0700

    add features

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -61,6 +61,8 @@ class Users extends Controller{
              // Hash Password
              $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
              if($this->userModel->register($data)){
+              flash('register_success', 'You are registered and can log in');
+            
               redirect('users/login');
             } else {
               die('Something went wrong');
@@ -87,6 +89,7 @@ class Users extends Controller{
   
           // Load view
           $this->view('users/register', $data);
+  
         }
 
       
@@ -115,12 +118,29 @@ class Users extends Controller{
           if(empty($data['password'])){
             $data['password_err'] = 'Please enter password';
           }
+
+          //check for user/email
+          if($this->userModel->findUserByEmail($data['email'])){
+            //user found
+          }else{
+            //No user found
+            $data['email_err']= 'No user found';
+          }
   
           // Make sure errors are empty
           if(empty($data['email_err']) && empty($data['password_err'])){
             // Validated
-            die('SUCCESS');
-          } else {
+           // check and set logged in user
+           $loggedInUser = $this->userModel->login($data['email'], $data['password']);
+
+           if($loggedInUser){
+            // Create Session
+            $this->createUserSession($loggedInUser);
+           }else{
+             $data['password_err'] = 'Password incorrect';
+             $this->view('users/login', $data);
+           }
+          }else{
             // Load view with errors
             $this->view('users/login', $data);
           }
@@ -139,4 +159,22 @@ class Users extends Controller{
           $this->view('users/login', $data);
         }
       }
+
+      public function createUserSession($user){
+        $_SESSION['user_id'] = $user->id;
+        $_SESSION['user_email'] = $user->email;
+        $_SESSION['user_name'] = $user->name;
+        redirect('posts');
+      }
+
+      
+    public function logout(){
+      unset($_SESSION['user_id']);
+      unset($_SESSION['user_email']);
+      unset($_SESSION['user_name']);
+      session_destroy();
+      redirect('users/login');
+    }
+
+ 
 }
